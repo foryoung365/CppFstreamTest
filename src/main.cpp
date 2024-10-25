86c9523789bd71925d736556b143d164f0864d46
foryoung365 <foryoung365@gmail.com>
2024-10-25 16:04:37 +0800

    add fopen test

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 void test_path(const char* path) {
     cout << "\nTesting path \"" << path << "\":" << endl;
-    
+
     {
         ifstream ifs(path);
         cout << "ifstream status:" << endl;
@@ -14,7 +14,7 @@ void test_path(const char* path) {
         cout << "  bad():  " << ifs.bad() << endl;
         cout << "  error: " << std::error_code(errno, std::system_category()).message() << endl;
     }
-    
+
     {
         fstream fs(path, ios::app);
         cout << "\nfstream status:" << endl;
@@ -23,26 +23,38 @@ void test_path(const char* path) {
         cout << "  bad():  " << fs.bad() << endl;
         cout << "  error: " << std::error_code(errno, std::system_category()).message() << endl;
     }
+
+    {
+        FILE* fp = nullptr;
+        int nErr = fopen_s(&fp, path, "r");
+        cout << "\nFILE* status:" << endl;
+        cout << "  good(): " << (fp != nullptr) << endl;
+        cout << "  error: " << nErr << endl;
+        if (fp)
+        {
+	        fclose(fp);
+        }
+    }
 }
 
 int main() {
-    #ifdef _WIN32
-        cout << "Running on Windows" << endl;
-    #elif __APPLE__
-        cout << "Running on macOS" << endl;
-    #elif __linux__
-        cout << "Running on Linux" << endl;
-    #else
-        cout << "Running on unknown OS" << endl;
-    #endif
+#ifdef _WIN32
+    cout << "Running on Windows" << endl;
+#elif __APPLE__
+    cout << "Running on macOS" << endl;
+#elif __linux__
+    cout << "Running on Linux" << endl;
+#else
+    cout << "Running on unknown OS" << endl;
+#endif
 
     cout << "Current working directory test:\n";
     test_path(".");
     test_path("./");
-    
-    #ifdef _WIN32
-        test_path(".\\");
-    #endif
+
+#ifdef _WIN32
+    test_path(".\\");
+#endif
 
     return 0;
 }
